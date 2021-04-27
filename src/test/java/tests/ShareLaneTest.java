f6c850c6dd7129f942fbc8a2b9e7160f2d2a7ff0
Alexander Gordeichik <a.gordeichik@invento.by>
2021-04-27 21:49:08 +0300

    Added comment2

--- src/test/java/tests/ShareLaneTest.java
+++ src/test/java/tests/ShareLaneTest.java
@@ -49,7 +49,7 @@ public class ShareLaneTest {
         driver.findElement(By.xpath("//*[@value='Register']")).click();
 // тут надо проверить результат111
         // feature_lesson5
-        // Added comment1
+        // Added comment12
 //        driver.quit();
     }
 }
